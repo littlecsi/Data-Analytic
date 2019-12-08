9803131d32e774b81b0195286b2ca86f6cc7a6b2
Lee <littlecsi27@gmail.com>
2019-12-08 15:51:31 +0900

    Made a disappointing 3D plot

--- script/crime_by_time_visualisation.R
+++ script/crime_by_time_visualisation.R
@@ -5,7 +5,9 @@ library(ggplot2)
 library(ggdark)
 library(reshape2)
 library(jsonlite)
-library(plot3D)
+
+library(plotly)
+library(car)
 
 ####################################################################################################
 ### Import
@@ -18,11 +20,30 @@ source("database/getDB.R")
 
 ####################################################################################################
 ##### Main
+cdf <- getColumns("SUB_CATE")
+
+# Finding the Frequency of each crime category
+crimeF <- table(cdf$SUB_CATE)
+#                     Var1   Freq
+# 9              CAR PROWL 148123
+# 26       THEFT-ALL OTHER  54239
+# 29        THEFT-SHOPLIFT  48623
+# 7   BURGLARY-RESIDENTIAL  46776
+# 17   MOTOR VEHICLE THEFT  43483
+crimeF <- as.data.frame(crimeF)
+crimeF <- crimeF[order(crimeF$Freq, decreasing=T),]
+crimeF <- crimeF[c(1:5),]
+
+# Plotting TOP 5 Crime Frequency
+crimePlot1 <- ggplot() + 
+  geom_col(aes(Var1, Freq, fill=Var1), crimeF, position="stack", show.legend=F) +
+  labs(x="Crime Subcategory", y="Frequency", title="TOP 5 Frequecy of Crime by Frequency between 2008 and 2018") +
+  theme(axis.line=element_line(color="Black", size=1, linetype="solid")) 
+crimePlot1
 
 # OCC_DATE, OCC_TIME, REP_DATE, REP_TIME, SUB_CATE, PRI_DESC, PRECINCT, SECTOR, BEAT, NEIGHBOR
-col <- c("OCC_TIME", "REP_TIME", "SUB_CATE", "BEAT")
+col <- c("OCC_TIME", "SUB_CATE", "BEAT")
 crimeData <- getColumns(col)
-dbDisconnectAll()
 
 ### Get Json File - Beat Area
 map <- fromJSON("database/seattle_beat.json")
@@ -37,26 +58,45 @@ beats <- intersect(beatJSON, beatCrime)
 crime_beat <- crimeData %>% subset(BEAT %in% beats)
 
 ##################################################
-##### 3D Plot - category, time, beat
+##### 3D Plot "Car Prowl" - category, time, beat
 
-### Categorize REP_TIME
+### Categorize OCC_TIME and RPE_TIME
 plot3dDF <- crime_beat[c("OCC_TIME", "SUB_CATE", "BEAT")]
-plot3dDF$TIME <- ifelse(nchar(plot3dDF$OCC_TIME) <= 2, 0, # 12AM ~ 1AM
-                  ifelse(nchar(plot3dDF$OCC_TIME) == 3, substr(plot3dDF$OCC_TIME, 1, 1), # 1AM ~ 12PM
-                    substr(plot3dDF$OCC_TIME, 1, 2))) # 12PM ~ 12AM
 
 ### Group Beats to Sectors
 plot3dDF$SECTOR <- substr(plot3dDF$BEAT, 1, 1)
 
-### Rearrange data frame and remove OCC_TIME column
-plot3dData <- plot3dDF[c("TIME", "SUB_CATE", "SECTOR")]
+### Subset "Car Prowl" category as it has the largest amount of crime frequency
+plot3dDF <- subset(plot3dDF, SUB_CATE != "")
 
-TIME <- plot3dData$TIME
-SUB_CATE <- plot3dData$SUB_CATE
-SECTOR <- plot3dData$SECTOR
+### Rearrange data frame and count occurances
+plot3dData <- plot3dDF[c("OCC_TIME", "SUB_CATE", "SECTOR")]
+catData <- as.data.frame(rename(count(plot3dData, OCC_TIME, SUB_CATE, SECTOR), FREQ=n))
 
-scatter3D(x=TIME, y=SUB_CATE, z=SECTOR)
+### Only treat data if frequency is higher than 10
+catData <- catData %>% subset(FREQ > 10)
+
+### Changing columns data into integers for plotting
+catData$SUB_CATE <- as.factor(catData$SUB_CATE)
+catData$SECTOR <- as.factor(catData$SECTOR)
+
+### 3D Scatter Plot with Color Scaling
+plot1 <- plot_ly(catData, x=~OCC_TIME, y=~SUB_CATE, z=~SECTOR, size=0.1,
+                 marker=list(color=~FREQ, colorscale=c("#FFE1A1", "#683531"), showscale=T)) %>% 
+  add_markers() %>% 
+  layout(scene=list(xaxis=list(title="Occured Time"),
+                    yaxis=list(title="SUB_CATE"),
+                    zaxis=list(title="SECTOR")),
+         annotations=list(
+           x=1.13, y=1.05,
+           text="Frequency of Crime",
+           xref="paper", yref="paper",
+           showarrow=F
+         ))
+plot1
 
 ##################################################
 ##### Crime by Beat visualisation
-crime_beat$SECTOR <- substr(crime_beat$BEAT, 1, 1)
+# crime_beat$SECTOR <- substr(crime_beat$BEAT, 1, 1)
+
+dbDisconnectAll()
