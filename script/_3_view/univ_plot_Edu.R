c1a992f4903333a7bcc1f38becd529066ad7e2d5
Get <dkdlwmzhs@naver.com>
2019-12-13 14:59:49 +0900

    12
    3123

--- script/_3_view/univ_plot_Edu.R
+++ script/_3_view/univ_plot_Edu.R
@@ -9,9 +9,15 @@ source("script/_1_database/getDB.R")
 # Print Edu Plot
 df_Edu <- getEduData()
 
-table(df_Edu$YEAR_N)
+# Create DF
+df_Edu_YearComp <- cbind(df_Edu$YEAR_N, df_Edu$COMPLETIONS) %>% as.data.frame(header = T)
+df_Edu_YearComp <- df_Edu_YearComp %>% as.factor(df_Edu_YearComp$V1)
+
+# table(df_Edu)
+colnames(df_Edu_YearComp) <- c("Year", "Completions")
 # 데이터 갯수
 # 2012 2013 2014 2015 2016 
 # 6759 6923 7084 7237 6950 
 
 # base ggplot :: boxplot / bar / line
+ggplot(data = df_Edu_YearComp, mapping = aes(x = Year, y = Compl, col = , fill = )) + geom_bar(stat = "identity")
